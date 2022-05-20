3d952f1b2bd3a95393b43089324cabc20fb9c75f
jamie082 <jamiemorrissey02@gmail.com>
2022-05-20 02:49:31 -0700

    added support for 200 status OK

--- scraper_site.py
+++ scraper_site.py
@@ -8,21 +8,24 @@ import requests
 
 # check for 200 status of web page
 
-check_http_status():
-
-    soup = BeautifulSoup(r.content, 'html.parser')
-    webpage = response.content
+def check_http_status():
     r = requests.get("http://bindfix.net/site/temp_site/frame_1.html")
+    soup = BeautifulSoup(r.content, 'html.parser')
+    webpage = r.content
     if r.status_code == 200:
         # if the request is successful return the HTML content
         return r.text
     else:
         raise Exception("an error occured while fetching html")
 
-for counter in soup.find_all('section', class_='column'):
-    class_counter = [a for a in counter.find_all('h1')]
-    for n, tag in enumerate(counter.find_all('div', class_='body_element')):
-        p_element = [x for x in tag.find_all('p', class_='body_element')] # paragraph element
-        print("Paragraph #1", class_counter[n].text.strip())
-        #print("Class Section", p_element[3].text.strip())
-        print()
+
+    for counter in soup.find_all('section', class_='column'):
+       class_counter = [a for a in counter.find_all('h1')]
+       for n, tag in enumerate(counter.find_all('div', class_='body_element')):
+            p_element = [x for x in tag.find_all('p', class_='body_element')] # paragraph element
+            print("Paragraph #1", class_counter[n].text.strip())
+            #print("Class Section", p_element[3].text.strip())
+            print()
+
+
+check_http_status()
