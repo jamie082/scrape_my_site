17ee6b3e2059bdd9b1ced49dc9bdb6cd0cd21cc3
jamie082 <jamiemorrissey02@gmail.com>
2022-05-20 13:47:34 -0700

    added more functions

--- scraper_site.py
+++ scraper_site.py
@@ -9,23 +9,25 @@ import requests
 # check for 200 status of web page
 
 def check_http_status():
-    response = requests.get("http://bindfix.net/site/temp_site/frame_good.html")
-    webpage = response.content
-    soup = BeautifulSoup(webpage, "html.parser")
     if response.status_code == 200:
         # if the request is successful return the HTML content
         return response.text
     else:
         raise Exception("an error occured while fetching html")
 
-response = requests.get("http://bindfix.net/site/temp_site/frame_good.html")
-webpage = response.content
-soup = BeautifulSoup(webpage, "html.parser")
+#check_http_status()
+    header_info()
+def start_program():
+    response = requests.get("http://bindfix.net/site/temp_site/frame_good.html")
+    webpage = response.content
+    soup = BeautifulSoup(webpage, "html.parser")
 
-for counter in soup.find_all('section', class_='column'):
-    class_counter = [a for a in counter.find_all('h1')]
-    for n, tag in enumerate(counter.find_all('div', class_='body_element')):
-        p_element = [x for x in tag.find_all('p', class_='body_element')] # paragraph element
-        print("Paragraph #1", class_counter[n].text.strip())
-        #print("Class Section", p_element[3].text.strip())
-        print()
+
+    for counter in soup.find_all('section', class_='column'):
+        class_counter = [a for a in counter.find_all('h1')]
+        for n, tag in enumerate(counter.find_all('div', class_='body_element')):
+            p_element = [x for x in tag.find_all('p', class_='body_element')] # paragraph element
+            print("Paragraph #1", class_counter[n].text.strip())
+            #print("Class Section", p_element[3].text.strip())
+            print()
+start_program()
